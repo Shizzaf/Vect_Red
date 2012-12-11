a47a4d7e22f41aa0c5f93c9d3508886fda45f9dc
Shizzaf <pandora_sk@ngs.ru>
2012-12-11 11:51:30 +0600

    DrowingCross
    Draw cross with classfile ClassShape and list shapes

---
+++ Kuznetsova/ClassShape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Kuznetsova
+{
+    public abstract class Shapes
+    {
+        public abstract void DrawWith(Graphics g, Pen p);
+    }
+    public class Cross : Shapes
+    {
+        int X, Y;
+        public Cross(int _X, int _Y)
+        {
+            X = _X;
+            Y = _Y;
+        }
+        public override void DrawWith(Graphics g, Pen p)
+        {
+            g.DrawLine(p, X - 3, Y - 3, X + 3, Y + 3);
+            g.DrawLine(p, X + 3, Y - 3, X - 3, Y + 3);
+        }
+    }
+}
